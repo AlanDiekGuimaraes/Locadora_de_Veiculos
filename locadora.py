34ff44ccf46a5b526db8320880969971f959f443
Alan Diek Guimarães <alandiekguimaraes@gmail.com>
2024-07-13 18:34:36 -0300

    Refatorando código

--- locadora.py
+++ locadora.py
@@ -14,12 +14,49 @@ veiculos_disponiveis = [
     ]
 veiculos_disponiveis.sort()
 veiculos_alugados =[]
+def mostrar_portfolio():
+    print(68 * '-')
+    mostrar_lista_de_veiculos(veiculos_disponiveis)
+    print(68 * '-')
+def alugar_um_veiculo():
+    print(68 * '-')
+    mostrar_lista_de_veiculos(veiculos_disponiveis)
+    print(68 * '-')
+    print('Escolha o código do veículo que deseja alugar.')
+    codigo_veiculo = int(input())
+    print('Por quantos dias você deseja alugar este veiculo?')
+    dias = int(input())
+
+    print(f'Você escolher {veiculos_disponiveis[codigo_veiculo][0]} por {dias} dias.')
+    print(f'O aluguel totalizaria R$ {dias * veiculos_disponiveis[codigo_veiculo][1]}. Deseja alugar?')
+    print('0 - SIM | 1 - NÃO')
+    confirmacao = int(input())
+
+    if confirmacao == 0:
+        print(f'Parabéns, você alugou o {veiculos_disponiveis[codigo_veiculo][0]} por {dias} dias')
+        veiculos_alugados.append(veiculos_disponiveis.pop(codigo_veiculo))
+
+
+def devolver_um_veiculo():
+    if len(veiculos_alugados) == 0:
+        print('Não há carros para devolver')
+    else:
+        print('Segue a lista de carros alugados. Qual você deseja devolver?')
+        mostrar_lista_de_veiculos(veiculos_alugados)
+        print('Escolha o código do carro que deseja devolver:')
+        codigo_veiculo_devolucao = int(input())
+        if confirmacao == 0:
+            print(f'Obrigado por devolver o veículo {veiculos_alugados[codigo_veiculo_devolucao][0]}')
+            veiculos_disponiveis.append(veiculos_alugados.pop(codigo_veiculo_devolucao))
+
 
 def mostrar_lista_de_veiculos(lista_de_veiculos):
 
     for contador, veiculos in enumerate(lista_de_veiculos):
         print(f'[{contador}] {veiculos[0]} - R$ {veiculos[1]} por dia.')
 
+
+# Inicio do programa
 print(68 * '-')
 print('Bem vindo a locadora BR'.center(68))
 print(68 * '-')
@@ -31,37 +68,12 @@ while True:
     try:
         operacao = int(input())
         if operacao == 0:
-            print(68 * '-')
-            mostrar_lista_de_veiculos(veiculos_disponiveis)
-            print(68 * '-')
+            mostrar_portfolio()
         elif operacao == 1:
-            print(68 * '-')
-            mostrar_lista_de_veiculos(veiculos_disponiveis)
-            print(68 * '-')
-            print('Escolha o código do veículo que deseja alugar.')
-            codigo_veiculo = int(input())
-            print('Por quantos dias você deseja alugar este veiculo?')
-            dias = int(input())
-
-            print(f'Você escolher {veiculos_disponiveis[codigo_veiculo][0]} por {dias} dias.')
-            print(f'O aluguel totalizaria R$ {dias * veiculos_disponiveis[codigo_veiculo][1]}. Deseja alugar?')
-            print('0 - SIM | 1 - NÃO')
-            confirmacao = int(input())
-
-            if confirmacao == 0:
-                print(f'Parabéns, você alugou o {veiculos_disponiveis[codigo_veiculo][0]} por {dias} dias')
-                veiculos_alugados.append(veiculos_disponiveis.pop(codigo_veiculo))
+            alugar_um_veiculo()
         elif operacao == 2:
-            if len(veiculos_alugados) == 0:
-                print('Não há carros para devolver')
-            else:
-                print('Segue a lista de carros alugados. Qual você deseja devolver?')
-                mostrar_lista_de_veiculos(veiculos_alugados)
-                print('Escolha o código do carro que deseja devolver:')
-                codigo_veiculo_devolucao = int(input())
-                if confirmacao == 0:
-                    print(f'Obrigado por devolver o veículo {veiculos_alugados[codigo_veiculo_devolucao][0]}')
-                    veiculos_disponiveis.append(veiculos_alugados.pop(codigo_veiculo_devolucao))
+            devolver_um_veiculo()
+
     except:
         print('ERRO, você digitou algum valor invalido, tente novamente!'.center(68))
         print('Reiniciando...')
